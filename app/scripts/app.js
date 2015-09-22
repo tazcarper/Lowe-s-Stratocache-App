5e08c346879611c078852a801df9a24fc33d7537
Taz Carper <jtc006@gmail.com>
2015-09-22 14:17:51 -0500

    Final update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
- angular
+angular
   .module('stratocacheApp', [
     'ngAnimate',
     'ngCookies',
@@ -20,22 +20,32 @@
     'hotspot',
     'productService'
   ])
-    .controller('global', ['$scope', '$location', '$rootScope',function($scope, $location, $rootScope) {
-      $scope.bodyClass='';
-      $scope.setTime = function(){
-       $rootScope.lastDigestRun = Date.now();
-       console.log($rootScope.lastDigestRun);
-      };
+  .controller('global', ['$scope', '$location', '$rootScope', function($scope, $location, $rootScope) {
+    $scope.bodyClass = '';
+    $scope.setTime = function() {
+      $rootScope.lastDigestRun = Date.now();
+    };
+    $scope.isSpanish = false;
 
-     
-    //  console.log($scope.bodyClass);
+    $scope.currentLocation = '';
     $rootScope.isActive = function(viewLocation) {
-    return viewLocation === $location.path();
+      if ($location.path().substr($location.path().length - 2).toString() === '_s') {
+        $scope.isSpanish = true;
+      } else {
+        $scope.isSpanish = false;
+      }
+      return viewLocation === $location.path();
+
+    };
+
+    $rootScope.sendHome = function() {
+      $location.path('/');
     };
 
 
-    }])
-  .config(function ($routeProvider) {
+
+  }])
+  .config(function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -47,49 +57,86 @@
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
-       .when('/cando', {
+      .when('/cando', {
         templateUrl: 'views/cando.html',
         controller: 'CandoCtrl',
         controllerAs: 'cando'
       })
-       .when('/getstarted',{
+      .when('/getstarted', {
         templateUrl: 'views/getStarted.html',
         controller: 'startedCtrl',
         controllerAs: 'started'
       })
-        .when('/faq',{
+      .when('/faq', {
         templateUrl: 'views/faq.html',
         controller: 'FaqCtrl',
         controllerAs: 'faq'
       })
-        .when('/productCat',{
+      .when('/productCat', {
         templateUrl: 'views/productCat.html',
         controller: 'ProductCtrl',
         controllerAs: 'product'
       })
-        .when('/servicePlans',{
+      .when('/servicePlans', {
         templateUrl: 'views/servicePlans.html',
         controller: 'ServicePlansCtrl',
         controllerAs: 'servicePlans'
+      })
+      .when('/main_s', {
+        templateUrl: 'views/main_s.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/cando_s', {
+        templateUrl: 'views/cando_s.html',
+        controller: 'CandoCtrl',
+        controllerAs: 'cando'
+      })
+      .when('/faq_s', {
+        templateUrl: 'views/faq.html',
+        controller: 'FaqCtrl',
+        controllerAs: 'faq'
+      })
+      .when('/productCat_s', {
+        templateUrl: 'views/productCat.html',
+        controller: 'ProductCtrl',
+        controllerAs: 'product'
+      })
+      .when('/servicePlans_s', {
+        templateUrl: 'views/servicePlans.html',
+        controller: 'ServicePlansCtrl',
+        controllerAs: 'servicePlans'
+      })
+      .when('/getstarted_s', {
+        templateUrl: 'views/getStarted.html',
+        controller: 'startedCtrl',
+        controllerAs: 'started'
       })
       .otherwise({
         redirectTo: '/'
       });
   })
-.run(function($rootScope, $location, $interval) {
+  .run(function($rootScope, $location, $interval) {
+
+    $rootScope.goHome = function() {
+      $location.path('/');
+    };
 
     $rootScope.lastDigestRun = Date.now();
-   $interval(function() {
+    $interval(function() {
       console.log('check idle');
-        var now = Date.now();            
-        if (now - $rootScope.lastDigestRun > 3*60*1000 && $location.path() !== '/') {
-           console.log('go home');
-           $location.path('/');
-        }
-    }, 29*1000);
+      var now = Date.now();
+      if (now - $rootScope.lastDigestRun > 3 * 60 * 1000 && $location.path() !== '/') {
+        console.log('go home');
+        $rootScope.goHome();
+      }
+    }, 29 * 1000);
+
+
 
     $rootScope.$on('$routeChangeStart', function() {
-        $rootScope.lastDigestRun = Date.now();  
+      $rootScope.lastDigestRun = Date.now();
+
     });
-  
-});
+
+  });
